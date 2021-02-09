86a065ae41b1a9b9d5757af3008f565b40aacd1c
luki <lukasz.diversityapps.pro@gmail.com>
2021-02-09 21:18:12 +0100

    added Weather

---
+++ src/main/kotlin/model/Weather.kt
@@ -0,0 +1,24 @@
+package model
+
+import javafx.beans.property.SimpleStringProperty
+import tornadofx.*
+import javax.json.JsonObject
+
+class Weather : JsonModel {
+    private val iconProperty = SimpleStringProperty()
+    private var icon by iconProperty
+
+    private val codeProperty = SimpleStringProperty()
+    private var code by codeProperty
+
+    private val descriptionProperty = SimpleStringProperty()
+    private var description by descriptionProperty
+
+    override fun updateModel(json: JsonObject) {
+        with(json) {
+            icon = getString("icon")
+            code = getString("code")
+            description = getString("description")
+        }
+    }
+}
