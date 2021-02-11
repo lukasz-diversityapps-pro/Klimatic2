7309f144a625d72ec97bc78ac5c1eb7e5fa7699c
luki <lukasz.diversityapps.pro@gmail.com>
2021-02-11 20:40:26 +0100

    updateModel for Data

--- src/main/kotlin/model/Data.kt
+++ src/main/kotlin/model/Data.kt
@@ -6,6 +6,7 @@ import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 import java.time.LocalDate
+import javax.json.JsonObject
 
 class Data : JsonModel {
     val validDateProperty = SimpleObjectProperty<LocalDate>()
@@ -14,132 +15,126 @@ class Data : JsonModel {
     val timestampProperty = SimpleIntegerProperty()
     var timestamp by timestampProperty
 
-    // "sunrise_ts": 1530331260,
     val sunriseTimestampProperty = SimpleIntegerProperty()
     var sunriseTimestamp by sunriseTimestampProperty
 
-    // "sunset_ts": 1530331260,
     val sunsetTimestampProperty = SimpleIntegerProperty()
     var sunsetTimestamp by sunsetTimestampProperty
 
-
-
-    // "weather": {}
     val weatherProperty = SimpleObjectProperty<Weather>()
     var weather by weatherProperty
 
-
-
-    // "snow": 10.45,
     val snowProperty = SimpleDoubleProperty()
     var snow by snowProperty
 
-    // "snow_depth": 45,
     val snowDepthProperty = SimpleIntegerProperty()
     var snowDepth by snowDepthProperty
 
-
-
-    // "precip": 1.1,
     val precipitationProperty = SimpleDoubleProperty()
     var precipitation by precipitationProperty
 
-    // pop: Probability of Precipitation (%)
     val probabilityOfPrecipitationProperty = SimpleIntegerProperty()
     var probabilityOfPrecipitation by probabilityOfPrecipitationProperty
 
-
-
-    // "temp": 1,
     val temperatureProperty = SimpleIntegerProperty()
     var temperature by temperatureProperty
 
-    // "max_temp": 1.5,
     val maxTempProperty = SimpleIntegerProperty()
     var maxTemp by maxTempProperty
 
-    // "min_temp": -1.23,
     val minTempProperty = SimpleIntegerProperty()
     var minTemp by minTempProperty
 
-    // "app_max_temp": 1.5,
     val apparentMaxTempProperty = SimpleIntegerProperty()
     var apparentMaxTemp by apparentMaxTempProperty
 
-    // "app_min_temp": -1.23,
     val apparentMinTempProperty = SimpleIntegerProperty()
     var apparentMinTemp by apparentMinTempProperty
 
-    // "high_temp": 1.5,
     val highTempProperty = SimpleIntegerProperty()
     var highTemp by highTempProperty
 
-    // "low_temp": -1.23,
     val lowTempProperty = SimpleIntegerProperty()
     var lowTemp by lowTempProperty
 
-
-
-    // "pres": 1005,
     val pressureProperty = SimpleIntegerProperty()
     var pressure by pressureProperty
 
     val seaLevelPressureProperty = SimpleIntegerProperty()
     var seaLevelPressure by seaLevelPressureProperty
 
-
-
-    // "clouds": 100,
     val cloudsProperty = SimpleIntegerProperty()
     var clouds by cloudsProperty
 
-    // clouds_low: Low-level (~0-3km AGL) cloud coverage (%)
     val cloudsLowProperty = SimpleIntegerProperty()
     var cloudsLow by cloudsLowProperty
 
-    // clouds_mid: Mid-level (~3-5km AGL) cloud coverage (%)
     val cloudsMidProperty = SimpleIntegerProperty()
     var cloudsMid by cloudsMidProperty
 
-    // clouds_hi: High-level (>5km AGL) cloud coverage (%)
     val cloudsHiProperty = SimpleIntegerProperty()
     var cloudsHi by cloudsHiProperty
 
-    // "vis": 3,
     val visibilityProperty = SimpleIntegerProperty()
     var visibility by visibilityProperty
 
-    // "rh": 95,
     val humidityProperty = SimpleIntegerProperty()
     var humidity by humidityProperty
 
-    // "dewpt": 1,
     val dewPointProperty = SimpleIntegerProperty()
     var dewPoint by dewPointProperty
 
-    // "uv": 6.5,
     val uvProperty = SimpleDoubleProperty()
     var uv by uvProperty
 
-
-
-    // "wind_spd": 13.85,
     val windSpeedProperty = SimpleDoubleProperty()
     var windSpeed by windSpeedProperty
 
-    // "wind_gust_spd"
     val windGustSpeedProperty = SimpleDoubleProperty()
     var windGustSpeed by windGustSpeedProperty
 
-    // "wind_dir": 105,
     val windDirectionProperty = SimpleIntegerProperty()
     var windDirection by windDirectionProperty
 
-    // "wind_cdir"
     val abbreviatedWindDirectionProperty = SimpleStringProperty()
     var abbreviatedWindDirection by abbreviatedWindDirectionProperty
 
-    // "wind_cdir_full"
     val verbalWindDirectionProperty = SimpleStringProperty()
     var verbalWindDirection by verbalWindDirectionProperty
+
+    override fun updateModel(json: JsonObject) {
+        with(json){
+            validDate = LocalDate.parse(getString("valid_date"))
+            timestamp = getInt("ts")
+            sunriseTimestamp = getInt("sunrise_ts")
+            sunsetTimestamp = getInt("sunset_ts")
+            weather = getJsonObject("weather").toModel()
+            snow = getDouble("snow")
+            snowDepth = getInt("snow_depth")
+            precipitation = getDouble("precip")
+            probabilityOfPrecipitation = getInt("pop")
+            temperature = getInt("temp")
+            maxTemp = getInt("max_temp")
+            minTemp = getInt("min_temp")
+            apparentMaxTemp = getInt("app_max_temp")
+            apparentMinTemp = getInt("app_min_temp")
+            highTemp = getInt("high_temp")
+            lowTemp = getInt("low_temp")
+            pressure = getInt("pres")
+            seaLevelPressure = getInt("slp")
+            clouds = getInt("clouds")
+            cloudsLow = getInt("clouds_low")
+            cloudsMid = getInt("clouds_mid")
+            cloudsHi = getInt("clouds_hi")
+            visibility = getInt("vis")
+            humidity = getInt("rh")
+            dewPoint = getInt("dewpt")
+            uv = getDouble("uv")
+            windSpeed = getDouble("wind_spd")
+            windGustSpeed = getDouble("wind_gust_spd")
+            windDirection = getInt("wind_dir")
+            abbreviatedWindDirection = getString("wind_cdir")
+            verbalWindDirection = getString("wind_cdir_full")
+        }
+    }
 }
